d0210741d7feb67f7946af5f0a600469d32935c5
AHdev67 <antoinehelfer.contact@gmail.com>
2024-02-09 09:41:30 +0100

    cleaned up indentation, added commentary

--- js/main.js
+++ js/main.js
@@ -1,28 +1,45 @@
+const cardContainer = document.querySelector("#todoCards");
+
 const card = document.querySelector(".todoCard");
-        const cardContainer = document.querySelector("#todoCards");
+const text = card.querySelector(".task");
+card.addEventListener("click", function(){
+    // clear text area on click.
+    text.value = ""; 
+});
         
-        const addBtn = document.querySelector("#btn");
-        addBtn.addEventListener("click", addCard);
+const addBtn = document.querySelector("#btn");
+//card addition.
+addBtn.addEventListener("click", addCard); 
 
-        const deleteBtn = document.querySelector(".delBtn");
-        deleteBtn.addEventListener("click", function(){
-            deleteCard(card)
-        });
+const deleteBtn = document.querySelector(".delBtn");
+deleteBtn.addEventListener("click", function(){
+    // card deletion.
+    deleteCard(card) 
+});
 
-        function addCard(){
-            let newCard = card.cloneNode(true);
+function addCard(){
+    //clones card div and it's content.
+    let newCard = card.cloneNode(true); 
 
-            let newText = newCard.querySelector(".task")
-            newText.value = "New Task";
+    let newText = newCard.querySelector(".task")
+    // resets default text display to "New task".
+    newText.value = "New Task"; 
+    newText.addEventListener("click", function(){
+        // clear text area on click, but for new cards.
+        newText.value = ""; 
+    });
 
-            let newDeleteBtn = newCard.querySelector(".delBtn");
-            newDeleteBtn.addEventListener("click", function(){
-                deleteCard(newCard);
-            });
+    let newDeleteBtn = newCard.querySelector(".delBtn");
+    newDeleteBtn.addEventListener("click", function(){
+        // ensures function of delete button for new cards.
+        deleteCard(newCard); 
+    });
 
-            cardContainer.appendChild(newCard);
-        }
+    // adds the new card in the container.
+    cardContainer.appendChild(newCard); 
+}
 
-        function deleteCard(elem){
-            elem.remove();
-        }
+function deleteCard(elem){
+    // anihilates the targeted element with nuclear fire.
+    elem.remove(); 
+}
